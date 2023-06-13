65fb6b0837d7c2e68d63457917bb9b4b8ae348e5
Bespin22 <bespin2222@gmail.com>
2023-06-13 10:39:22 +0530

    hy

--- TOURISTPLACEDETAILS/src/app/app.component.spec.ts
+++ TOURISTPLACEDETAILS/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'LOGINREGISTER'`, () => {
+  it(`should have as title 'TOURISTPLACEDETAILS'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('LOGINREGISTER');
+    expect(app.title).toEqual('TOURISTPLACEDETAILS');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('LOGINREGISTER app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('TOURISTPLACEDETAILS app is running!');
   });
 });
--- TOURISTPLACEDETAILS/src/app/app.module.ts
+++ TOURISTPLACEDETAILS/src/app/app.module.ts
@@ -1,21 +1,21 @@
 import { NgModule } from '@angular/core';
+import { Component } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClient } from '@angular/common/http';
+import { NgForm } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
-import { RegisterComponent } from './register/register.component';
-import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent,
-    RegisterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,FormsModule,
+    AppRoutingModule,Component,HttpClient,
+    NgForm,FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
